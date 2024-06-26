8747e0662115e821395784e02c79e2b787b8a128
hussein <husseinsedoum@gmail.com>
2024-06-26 23:07:10 +0000

    add fixing into stripe checkout issue and improve checkout button with loading animation

---
+++ app/api/webhook/route.ts
@@ -0,0 +1,54 @@
+import Stripe from 'stripe'
+import { headers } from 'next/headers'
+import { NextResponse } from 'next/server'
+import { stripe } from '@/lib/stripe'
+import prismaDB from '@/lib/prismaClient'
+
+export async function POST(req: Request) {
+  const body = await req.text()
+  const signature = headers().get('Stripe-signature') as string
+  let event: Stripe.Event
+  try {
+    event = stripe.webhooks.constructEvent(body, signature, process.env.STRIPE_WEB_HOOK)
+  } catch (error: any) {
+    return new NextResponse(`webhook error: ${error.message}`, { status: 400 })
+  }
+
+  const session = event.data.object as Stripe.Checkout.Session
+  const address = session?.customer_details.address
+  const addressArray = [address?.line1, address?.line2, address?.city, address?.state, address?.postal_code, address?.country]
+
+  const addressString = addressArray.filter((loc) => loc !== null).join(',')
+
+  if (event.type === 'checkout.session.completed') {
+    const order = await prismaDB.order.update({
+      where: {
+        id: session?.metadata?.orderId
+      },
+      data: {
+        isPaid: true,
+        address: addressString,
+        phone: session?.customer_details?.phone || ''
+      },
+      include: {
+        orderItems: true
+      }
+    })
+
+    const productsIds = order.orderItems.map((orderIem) => orderIem?.productCode)
+    console.log(productsIds)
+
+    await prismaDB.product.updateMany({
+      where: {
+        id: {
+          in: [...productsIds]
+        }
+      },
+      data: {
+        isArchived: true
+      }
+    })
+  }
+
+  new NextResponse(null, {status:200})
+}
