04093e3d3fe8030f78dd782ecad11a88bc461f3f
hussein <husseinsedoum@gmail.com>
2024-10-28 12:45:28 +0000

    fro testing user info

--- components/Navigation/UserInfoWrap.tsx
+++ components/Navigation/UserInfoWrap.tsx
@@ -1,11 +1,14 @@
 'use client';
+import { SessionProvider } from 'next-auth/react';
 
 import UserInfo from '@/components/Navigation/UserInfo'
 
 const UserInfoWrap = ({session}) => {
   return (
+    <SessionProvider>
       <UserInfo  session={session}
       />
+      </SessionProvider>
   );
 };
 
