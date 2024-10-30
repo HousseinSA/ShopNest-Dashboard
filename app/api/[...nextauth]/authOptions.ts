0df41ed6bc9c522bc21e770de7172275dd82f1fa
hussein <husseinsedoum@gmail.com>
2024-10-30 15:48:16 +0000

    remove domain prop

--- app/api/[...nextauth]/authOptions.ts
+++ app/api/[...nextauth]/authOptions.ts
@@ -34,9 +34,6 @@ export const authOptions: NextAuthOptions = {
         sameSite: 'none',
         path: '/',
         secure: true,
-        domain: process.env.NODE_ENV === 'production' 
-          ? process.env.COOKIE_DOMAIN    // Add this to your env variables
-          : undefined
       },
     },
   },
