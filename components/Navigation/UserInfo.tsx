b7c016792ccce91280f767afb5d6d20cf54ccc6d
hussein <husseinsedoum@gmail.com>
2024-10-31 17:11:41 +0000

    revert back to old next-auth way

--- components/Navigation/UserInfo.tsx
+++ components/Navigation/UserInfo.tsx
@@ -5,7 +5,7 @@ import { signOut } from 'next-auth/react'
 import { User2Icon } from 'lucide-react'
 import Image from 'next/image'
 import ClipLoader from 'react-spinners/ClipLoader'
-import getUserSession from '@/lib/getUserSession'; 
+// import getUserSession from '@/lib/getUserSession'; 
 
 const UserInfo = ({ session }) => {
   const [isOpen, setIsOpen] = useState(false)
@@ -19,28 +19,27 @@ const UserInfo = ({ session }) => {
     setIsOpen((prev) => !prev)
   }
 
-  const [user, setUser] = useState(null);
-
+  // const [user, setUser] = useState(null);
+  console.log('testing session',session)
     
-  useEffect(() => {
-    const fetchUserSession = async () => {
-      const userSession = await getUserSession();
-      console.log('Testing user session:', userSession); // Log the user session data
-      if (userSession && userSession.user) {
-        setUser(userSession.user); // Set user data if available
-      }
+  // useEffect(() => {
+  //   const fetchUserSession = async () => {
+  //     const userSession = await getUserSession();
+  //     console.log('Testing user session:', userSession); // Log the user session data
+  //     if (userSession && userSession.user) {
+  //       setUser(userSession.user); // Set user data if available
+  //     }
 
-    };
+  //   };
 
-    fetchUserSession(); // Fetch user session on component mount
-  }, []);
+  //   fetchUserSession(); // Fetch user session on component mount
+  // }, []);
 
   return (
     <div className='relative z-30'>
       <div onClick={toggleMenu} className='flex items-center justify-center cursor-pointer rounded-full bg-primary hover:primary-foreground transition duration-300 w-8 h-8 md:h-11 md:w-11'>
         {session?.user?.image ? <Image src={session.user.image} alt='User image' width={18} height={18} className='rounded-full w-full h-full' /> : <User2Icon size={18} color='white' />}
       </div>
-
       {/* Dropdown menu without animations */}
       {isOpen && (
         <div className='absolute right-0 mt-2 w-48 bg-white shadow-lg rounded-lg overflow-hidden z-40'>
