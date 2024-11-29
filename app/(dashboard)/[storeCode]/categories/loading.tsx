e2e86af0d9a84d5af7287bdecf2acd89c8a70e78
hussein <husseinsedoum@gmail.com>
2024-11-29 16:24:59 +0000

    fix some layout issue in dashboard

--- app/(dashboard)/[storeCode]/categories/loading.tsx
+++ app/(dashboard)/[storeCode]/categories/loading.tsx
@@ -1,24 +1,24 @@
-import React from 'react'
+import React from 'react';
 
 const Loading = () => {
-  const skeletons = Array(5).fill(null)
+  const skeletons = Array(5).fill(null);
 
   return (
     <div className='flex flex-col space-y-4 p-8 pt-6'>
-     <div className='skeleton-text' style={{ height: '1.2rem', width: '30%',  }}></div>
-     <div className='skeleton-text' style={{ height: '1rem', width: '20%',  }}></div>
-      <div className='skeleton-text skeleton' style={{ height: '1rem', width: '30%', }}></div>
-      <div className='flex flex-col skeleton'>
+      <div className='skeleton-text' style={{ height: '1.2rem', width: '30%' }}></div>
+      <div className='skeleton-text' style={{ height: '1rem', width: '20%' }}></div>
+      <div className='skeleton-text skeleton' style={{ height: '1rem', width: '30%' }}></div>
+      <div className='flex flex-col w-full skeleton'> {/* Ensure this div expands */}
         {skeletons.map((_, index) => (
-              <div key={index} className=' grid gap-4 grid-cols-3  py-6 place-items-center'>
-              <div className='skeleton-text' style={{ height: '.6rem', width: '80%' }}></div>
-              <div className='skeleton-text' style={{ height: '.6rem', width: '80%' }}></div>
-              <div className='skeleton-text' style={{ height: '.6rem', width: '80%' }}></div>
-            </div>
+          <div key={index} className='grid gap-4 grid-cols-3 py-6 place-items-center w-full'>
+            <div className='skeleton-text' style={{ height: '.6rem', width: '100%' }}></div> {/* Set width to 100% */}
+            <div className='skeleton-text' style={{ height: '.6rem', width: '100%' }}></div> {/* Set width to 100% */}
+            <div className='skeleton-text' style={{ height: '.6rem', width: '100%' }}></div> {/* Set width to 100% */}
+          </div>
         ))}
       </div>
     </div>
-  )
+  );
 }
 
-export default Loading
+export default Loading;
